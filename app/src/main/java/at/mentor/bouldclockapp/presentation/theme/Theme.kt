e7ca082e6bb17dafbbe34a0c8a65e2d30e40c22f
MentorDerGrosse <90867261+MentorDerGrosse@users.noreply.github.com>
2026-09-20 21:18:47 +0200

    Initial commit

---
+++ app/src/main/java/at/mentor/bouldclockapp/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package at.mentor.bouldclockapp.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun BouldClockAppTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
